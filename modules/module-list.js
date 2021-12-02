67ba561a14c2ac55c10dc8c8e55ff39a96d9c6cb
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-12-02 16:32:08 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -5,8 +5,9 @@
     var m=$vm.module_list;
     var api="wimr";
     //-------------------------------------------------------------------------------------
-    m[p+"research-studies"] ={url:H+"/business/research-studies/page.html",form_module:'jsonv',prefix:p};
-    m[p+"contacts"]         ={url:H+"/business/contacts/page.html",form_module:'jsonv',prefix:p};
+    m[p+"research-studies"]     ={url:H+"/business/research-studies/page.html",form_module:'jsonv',prefix:p};
+    m[p+"contacts"]             ={url:H+"/business/contacts/page.html",form_module:'jsonv',prefix:p};
+    m[p+"contacts-newsletters"] ={url:H+"/business/contacts-newsletters/page.html",form_module:'jsonv',prefix:p};
     
     m[p+"number-of-sleep-study-by-year-and-month"]={url:H+"/business/number-of-sleep-study-by-year-and-month/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
     m[p+"number-of-sleep-study-by-year"]={url:H+"/business/number-of-sleep-study-by-year/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
