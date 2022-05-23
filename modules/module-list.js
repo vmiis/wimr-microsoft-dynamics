5b0adcf357561555532d4fd2b59bd51c4c9aaa6f
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-05-23 17:47:03 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -7,6 +7,7 @@
     //-------------------------------------------------------------------------------------
     m[p+"research-studies"]     ={url:H+"/business/research-studies/page.html",form_module:'jsonv',prefix:p};
     m[p+"contacts"]             ={url:H+"/business/contacts/page.html",form_module:'jsonv',prefix:p};
+    m[p+"flows"]                ={url:H+"/business/flows/page.html",form_module:'jsonv',prefix:p};
     m[p+"contacts-newsletters"] ={url:H+"/business/contacts-newsletters/page.html",form_module:'jsonv',prefix:p};
     m[p+"contacts-newsletters-2019"] ={url:H+"/business/contacts-newsletters-2019/page.html",form_module:'jsonv',prefix:p};
     m[p+"contacts-newsletters-2020"] ={url:H+"/business/contacts-newsletters-2020/page.html",form_module:'jsonv',prefix:p};
@@ -14,4 +15,10 @@
     
     m[p+"number-of-sleep-study-by-year-and-month"]={url:H+"/business/number-of-sleep-study-by-year-and-month/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
     m[p+"number-of-sleep-study-by-year"]={url:H+"/business/number-of-sleep-study-by-year/page.html",Table:'flow-activity',form_module:'jsonv',prefix:p,router:1};
+    m[p+"solutions"]             ={url:H+"/business/solutions/page.html",form_module:'jsonv',prefix:p};
+    m[p+"can-rest"]              ={url:H+"/business/contact-views/can-rest/page.html",form_module:'jsonv',prefix:p};
+    m[p+"can-rest-team"]         ={url:H+"/business/contact-team/can-rest/page.html",form_module:'jsonv',prefix:p};
+    m[p+"can-rest-join"]         ={url:H+"/business/contact-join/can-rest/page.html",form_module:'jsonv',prefix:p};
+    m[p+"user-roles"]            ={url:H+"/business/records/user-roles/page.html",form_module:'jsonv',prefix:p};
+    m[p+"user-license"]          ={url:H+"/business/records/user-license/page.html",form_module:'jsonv',prefix:p};
 })();
